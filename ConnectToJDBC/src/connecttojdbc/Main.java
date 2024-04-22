d999e646602a65e745160ca1f1bf52d202ec1496
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-04-22 14:41:35 +0100

    :construction: Adding connection

--- ConnectToJDBC/src/connecttojdbc/Main.java
+++ ConnectToJDBC/src/connecttojdbc/Main.java
@@ -4,7 +4,25 @@ package connecttojdbc;
 public class Main {
 
   public static void main(String[] args) {
-        System.out.println("Hello, aren't I marvellous!? Hello");
+      
+      //setup some variables (e.g., password, url, name, dbname)
+      String url = "jdbc:mysql://localhost:3306/";
+      String dbName = "2023_ds1_ca3_games"; //any DB you have!
+      
+      String userName = "root";
+      String password = "";
+      
+      
+      //open a connection
+      
+      //execute a query
+      
+      //show the results
+      
+      //close the connection      
+      
+      
+        
     }
     
 }
