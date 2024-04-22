80086e9922c2099c519ba81987a4bce3d4a52e0a
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-04-22 15:15:30 +0100

    Update Main.java

--- ConnectToJDBC/src/connecttojdbc/Main.java
+++ ConnectToJDBC/src/connecttojdbc/Main.java
@@ -15,7 +15,7 @@ public class Main {
       
       //setup some variables (e.g., password, url, name, dbname)
       String url = "jdbc:mysql://localhost:3306/";
-      String dbName = "2023_ds1_ca3_games"; //any DB you have!
+      String dbName = "2024_ds1_ca3_games"; //any DB you have!
       
       String userName = "root";
       String password = "";
