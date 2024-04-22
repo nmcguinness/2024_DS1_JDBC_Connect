64894da5430bed3db24f2033af047caab32bebd1
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-04-22 15:37:54 +0100

    :sparkles: Added DBConnect

--- ConnectToJDBC/src/connecttojdbc/DBConnect.java
+++ ConnectToJDBC/src/connecttojdbc/DBConnect.java
@@ -3,14 +3,21 @@ package connecttojdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBConnect {
     
+    //no member variables - no state
+   // private string someUsefulVar;
+   // private int connectSpeed;
+    
 
-    public Connection connect(String url,
+    public static Connection connect(String url,
             String dbName, String userName,
             String password)
     {
+    
         Connection conn = null;       
          try{
           conn = DriverManager.getConnection(url + dbName,
@@ -23,5 +30,19 @@ public class DBConnect {
     }
     
     //disconnect
+    public static boolean disconnect(Connection conn){
+
+      try {
+          conn.close();
+          return true;
+      } 
+      catch (SQLException ex) {
+          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      
+      return false;
+    }
+
+    
     
 }
--- ConnectToJDBC/src/connecttojdbc/Main.java
+++ ConnectToJDBC/src/connecttojdbc/Main.java
@@ -10,8 +10,8 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 public class Main {
-
-  public static void main(String[] args) {
+    
+     public static void main(String[] args) {
       
       //setup some variables (e.g., password, url, name, dbname)
       String url = "jdbc:mysql://localhost:3306/";
@@ -20,53 +20,72 @@ public class Main {
       String userName = "root";
       String password = "";
       
-      Connection conn = null;
+      //connect to DB
+      Connection conn 
+       = DBConnect.connect(url, dbName, userName, password);
       
-      try{
-          //open a connection
-          conn = DriverManager.getConnection(url + dbName,
-                  userName, password);
-      }
-      catch(SQLException e){
-          System.out.println(e.getMessage());
-      }
-      catch(Exception e)
-      {
-          System.out.println(e.getMessage());
-          System.out.println("I dont know but something bad happened!");
-      }
+      //disconnect from DB
+      DBConnect.disconnect(conn);
       
-      try{
-        //execute a query
-        Statement statement = conn.createStatement();
-
-        //form the query
-        String query = "SELECT * FROM games"; //use your table name!
-
-        //show the results
-        ResultSet resultSet = statement.executeQuery(query);
-
-
-        while(resultSet.next())
-        {
-        System.out.println(resultSet.getInt("GameID"));
-        System.out.println(resultSet.getString("GameName"));
-        System.out.println(resultSet.getDate("ReleaseDate"));
-        System.out.println(resultSet.getString("Genre"));
-        }
-      }
-      catch(SQLException e)
-      {
-          System.out.println(e.getMessage());
-      }
-        
-       //close the connection  
-      try {
-          conn.close();
-      } catch (SQLException ex) {
-          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-      }
 
     }
+
+//  public static void main(String[] args) {
+//      
+//      //setup some variables (e.g., password, url, name, dbname)
+//      String url = "jdbc:mysql://localhost:3306/";
+//      String dbName = "2024_ds1_ca3_games"; //any DB you have!
+//      
+//      String userName = "root";
+//      String password = "";
+//      
+//      Connection conn = null;
+//      
+//      try{
+//          //open a connection
+//          conn = DriverManager.getConnection(url + dbName,
+//                  userName, password);
+//      }
+//      catch(SQLException e){
+//          System.out.println(e.getMessage());
+//      }
+//      catch(Exception e)
+//      {
+//          System.out.println(e.getMessage());
+//          System.out.println("I dont know but something bad happened!");
+//      }
+//      
+//      try{
+//        //execute a query
+//        Statement statement = conn.createStatement();
+//
+//        //form the query
+//        String query = "SELECT * FROM games"; //use your table name!
+//
+//        //show the results
+//        ResultSet resultSet = statement.executeQuery(query);
+//
+//
+//        while(resultSet.next())
+//        {
+//        System.out.println(resultSet.getInt("GameID"));
+//        System.out.println(resultSet.getString("GameName"));
+//        System.out.println(resultSet.getDate("ReleaseDate"));
+//        System.out.println(resultSet.getString("Genre"));
+//        }
+//      }
+//      catch(SQLException e)
+//      {
+//          System.out.println(e.getMessage());
+//      }
+//        
+//       //close the connection  
+//      try {
+//          conn.close();
+//      } catch (SQLException ex) {
+//          Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//      }
+//
+//    }
     
 }
